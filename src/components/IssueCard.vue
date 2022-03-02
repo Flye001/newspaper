fb6058e64f9bb99b46a3e0b467499929b0d2af43
Flye001 <philip.org1@gmail.com>
2022-03-02 20:35:25 +0000

    initial commit

---
+++ src/components/IssueCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row mt-5 justify-content-center">
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+            <div class="cursor" @click="ToggleDropdown">
+                <h5 class="card-title">{{Data.Month}} {{Data.Year}} Issue</h5>
+            </div>
+
+            <ul class="list-group list-group-flush" v-if="Dropdown">
+                <li class="list-group-item cursor"
+                v-for="item in Data.Items" :key="item"
+                @click="say('An article about ' + item.Name + ' suddenly appears!')">{{item.Name}}</li>
+           </ul>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+.cursor {
+    cursor: pointer;
+}
+</style>
+
+<script>
+export default {
+  props: ['Data'],
+  data () {
+    return {
+      Dropdown: false
+    }
+  },
+  methods: {
+    ToggleDropdown () {
+      this.Dropdown = !this.Dropdown
+    },
+    say (text) {
+      alert(text)
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,97 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://www.keaston.bham.sch.uk/_site/images/design/logo.png" alt="" width="28" height="30" class="d-inline-block align-text-top">
+      Aston School Newspaper
+    </a>
+  </div>
+</nav>
+
+  <div class="container-float">
+    <div class="row">
+          <h1 class="my-3">Aston School Newspaper</h1>
+    </div>
+
+    <!-- <IssueCard
+    v-for="Issue in Issues"
+    :key="Issue"
+    :Data="Issue"
+    ></IssueCard> -->
+
+  <br>
+
+  <div class="row">
+
+    <div class="col col-12 col-sm-4 col-md-3">
+<div class="card mb-3 mx-2">
+  <img src="https://www.aljazeera.com/wp-content/uploads/2022/02/2022-02-15T040103Z_157749486_RC29FS9GHAYB_RTRMADP_3_UKRAINE-CRISIS-RUSSIA-GERMANY.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">A Recap of Russian Conflict with Ukraine</h5>
+    <p class="card-text">Russia's invasion of Ukraine is ongoing. This article will resultantly be incomplete, with a comprehensive recap of events only being possible after the fact.</p>
+    <p class="card-text"><small class="text-muted">Kai</small></p>
+    <a class="btn btn-sm btn-primary">Read More</a>
+  </div>
+</div>
+    </div>
+
+    <div class="col col-12 col-sm-4 col-md-3">
+<div class="card mb-3 mx-2">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">News</h5>
+    <p class="card-text">So this could maybe be like the article summed up in a sentance or 2, or just the first sentance of the article etc.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <a class="btn btn-sm btn-primary">Read More</a>
+  </div>
+</div>
+    </div>
+
+    <div class="col col-12 col-sm-4 col-md-3">
+<div class="card mb-3 mx-2">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">News</h5>
+    <p class="card-text">So this could maybe be like the article summed up in a sentance or 2, or just the first sentance of the article etc.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <a class="btn btn-sm btn-primary">Read More</a>
+  </div>
+</div>
+    </div>
+
+    <div class="col col-12 col-sm-4 col-md-3">
+<div class="card mb-3 mx-2">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">News</h5>
+    <p class="card-text">So this could maybe be like the article summed up in a sentance or 2, or just the first sentance of the article etc.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    <a class="btn btn-sm btn-primary">Read More</a>
+  </div>
+</div>
+    </div>
+
+  </div>
+
+    <!-- <IssueCard Month="February"></IssueCard>
+    <IssueCard Month="January"></IssueCard>
+    <IssueCard Month="December"></IssueCard> -->
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import IssueCard from '@/components/IssueCard.vue'
+import RawIssues from '@/issues.json'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  // components: {
+  //   IssueCard
+  // },
+  data () {
+    return {
+      Issues: RawIssues.Issues
+    }
   }
 }
 </script>
