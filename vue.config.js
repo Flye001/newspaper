b1bfbddb018b2de22b02df02071b1c15759ef403
Flye001 <philip.org1@gmail.com>
2022-03-12 20:58:08 +0000

    deleted pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  chainWebpack: config => {
+    config.plugins.delete('pwa')
+    config.plugins.delete('workbox')
+  },
   outputDir: "H:/Programming/Websites/school-newspaper/docs",
   transpileDependencies: true
 })
