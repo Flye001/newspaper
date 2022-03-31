1c5a16d7b6412885057b1014119d900cc3f05434
Flye001 <philip.org1@gmail.com>
2022-03-31 08:58:22 +0100

    updated home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,9 @@
 
       <h1 class="text-3xl">Welcome to the Aston Digest!</h1>
 
-      <p>Some about us text goes here...</p>
+      <p>We are a student run newspaper from King Edward Aston school, founded in 2022. We cover everything from world events, Science, and local School news; and all our articles are written by students, we allow any student to submit articles.</p>
 
-      <p class="py-5">Why not read out latest article:</p>
+      <p class="py-5">Why not read our latest article:</p>
 
       <ArticleCard
       :Article="AllArticles[0]"></ArticleCard>
